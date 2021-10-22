a2c7d7426a52ad28a659221bb79e3169968f1362
rjlaflin <rjlaflin@uwm.edu>
2021-10-22 10:47:28 -0500

    hopefully fixed secret key issue...

--- capstone_project/models.py
+++ capstone_project/models.py
@@ -9,12 +9,11 @@ class User(models.Model):
         Staff = '1', _('Staff')
         Patient = '2', _('Patient')
 
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     unique_id = models.CharField(max_length=50)
     pwd = models.CharField(max_length=30)
     user_type = models.IntegerField(
-        max_length=1,
         choices=UserType.choices,
         default=UserType.Patient
     )
@@ -22,7 +21,6 @@ class User(models.Model):
 
 
 class StatusTable(models.Model):
-
     NotComplete = 0
     NotAdequate = 1
     Adequate = 2
